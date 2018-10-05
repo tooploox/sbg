92c486ccad2a8d6a6dc00daf9ce98225b6bc2d3f
Karol Wieczorek <karol.wieczorek@outlook.com>
2018-10-05 14:59:42 +0200

    Added Quick and Nimble to a project

--- Package.swift
+++ Package.swift
@@ -7,7 +7,9 @@ let package = Package(
     name: "sbg",
     dependencies: [
         .package(url: "https://github.com/stencilproject/Stencil.git", .upToNextMinor(from: "0.13.0")),
-        .package(url: "https://github.com/tuist/xcodeproj.git", .upToNextMajor(from: "6.0.0"))
+        .package(url: "https://github.com/tuist/xcodeproj.git", .upToNextMajor(from: "6.0.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from:"7.3.1")),
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "1.3.2"))
     ],
     targets: [
         .target(
@@ -15,6 +17,6 @@ let package = Package(
             dependencies: ["Stencil", "xcodeproj"]),
         .testTarget(
             name: "sbgTests",
-            dependencies: ["sbg"]),
+            dependencies: ["sbg", "Quick", "Nimble"]),
     ]
 )
