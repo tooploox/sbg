160ced0e469685dd4a31c9d285dfafc92e17b36e
Pawel Chmiel <pawel.chmiel@tooploox.com>
2018-10-11 08:57:22 +0200

    parsing commandLine parameters in to a dictionary

--- Sources/SBGCore/ApplicationParameters.swift
+++ Sources/SBGCore/ApplicationParameters.swift
@@ -7,4 +7,27 @@ import Foundation
 struct ApplicationParameters {
     let generatorName: String
     let generatorParameters: [String: String]
+    
+    init(generatorName: String, generatorParameters: [String: String]) {
+        self.generatorName = generatorName
+        self.generatorParameters = generatorParameters
+    }
+    
+    init(_ commandLineParameters: [String]) {
+        let parameters = ApplicationParameters.convert(commandLineParameters)
+        
+        generatorName = parameters["--name"] ?? ""
+        generatorParameters = parameters
+    }
+    
+    private static func convert(_ commandLineParameters: [String]) -> [String : String] {
+        guard !commandLineParameters.isEmpty else { return [:] }
+        
+        var dictionary = [String: String]()
+        _ = stride(from: 0, to: commandLineParameters.count, by: 2).map { index in
+            dictionary[commandLineParameters[index]] = commandLineParameters[index + 1]
+        }
+        
+        return dictionary
+    }
 }
