0fefbeebffd481b0b38071f6862545ec372fb22f
Karol Wieczorek <karol.wieczorek@outlook.com>
2018-10-18 18:53:54 +0200

    Implemented FoundationConfigParser

---
+++ Sources/SBGCore/Configuration/ConfigFileParser.swift
@@ -0,0 +1,36 @@
+//
+// Created by Karol on 18/10/2018.
+//
+
+import Foundation
+
+protocol FileReader {
+    func read(file: String) -> Data?
+}
+
+enum ConfigFileParserError: Error, Equatable {
+    case cannotReadFile(String)
+    case cannotParseData(Data)
+}
+
+class FoundationConfigFileParser {
+
+    private let fileReader: FileReader
+
+    init(fileReader: FileReader) {
+        self.fileReader = fileReader
+    }
+
+    func parse(file: String) -> Result<[String: String], ConfigFileParserError> {
+
+        guard let fileData = fileReader.read(file: file) else {
+            return .failure(.cannotReadFile(file))
+        }
+
+        guard let fileDictionary = (try? JSONSerialization.jsonObject(with: fileData)) as? [String: String] else {
+            return .failure(.cannotParseData(fileData))
+        }
+
+        return .success(fileDictionary)
+    }
+}
