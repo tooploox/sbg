9bb815dede9ba1201f66dea9a5339047760bf371
Karolina Samorek <karolina.samorek@tooploox.com>
2018-11-05 10:50:12 +0100

    Add stencil files content

--- Sources/SBGCore/EnvironmentInitialization/DemoFilesContentProvider.swift
+++ Sources/SBGCore/EnvironmentInitialization/DemoFilesContentProvider.swift
@@ -8,22 +8,67 @@ final class DemoFilesContentProvider: FilesContentProvider {
 
     private(set) var sbgConfigFileContent: String = """
                                                     {
-                                                        "main_target": "Your project target",
-                                                        "connectors_path": "Your project name/Connectors",
-                                                        "presenters_path": "Your project name/Presenters",
-                                                        "view_controllers_path": "Your project name/Controllers"
+                                                        "main_target": "your_project_main_target",
+                                                        "connectors_path": "your_project_name/Connectors",
+                                                        "presenters_path": "your_project_name/Presenters",
+                                                        "view_controllers_path": "your_project_name/Controllers"
                                                     }
                                                     """
 
     private(set) var templatesFiles: [String: String] = [
         "connector.stencil": """
+                             import UIKit
 
+                             class {{module_name}}Connector: {{module_name}}Routable {
+
+                                 func connect() -> UIViewController {
+                                     let presenter = {{module_name}}Presenter(router: self)
+                                     let viewController = {{module_name}}ViewController(presenter: presenter)
+                                     presenter.view = viewController
+
+                                     return viewController
+                                 }
+                             }
                              """,
         "presenter.stencil": """
+                             import Foundation
 
+                             protocol {{module_name}}View: class {
+
+                             }
+
+                             protocol {{module_name}}Routable {
+
+                             }
+
+                             final class {{module_name}}Presenter {
+
+                                 weak var view: {{module_name}}View?
+
+                                 private let router: {{module_name}}Routable
+
+                                 init(router: {{module_name}}Routable) {
+                                    self.router = router
+                                 }
+
+                             }
                              """,
         "view_controller.stencil": """
+                                   import UIKit
 
+                                   class {{module_name}}ViewController: ViewController, {{module_name}}View {
+
+                                       private let presenter: {{module_name}}Presenter
+
+                                       init(presenter: {{module_name}}Presenter) {
+                                           self.presenter = presenter
+                                           super.init(nibName: nil, bundle: nil)
+                                       }
+
+                                       required init?(coder aDecoder: NSCoder) {
+                                           fatalError("init coder not implemented")
+                                       }
+                                   }
                                    """
     ]
 
